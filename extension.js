fbfbada2e27e5ca3898cf46cfa2716c4f8b24efe
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2022-02-04 00:27:08 -0500

    improvement on the code

--- extension.js
+++ extension.js
@@ -1,82 +1,87 @@
-location.reload();
-let humanScore = 0;
-let computerScore = 0;
-let draw = 0;
+const choices = ["rock","paper","scissors"]
+const winners = []
 
-// variables containing messages to be displayed at the end of each round
-let humanWinsRound = "You win this round!";
-let computerWinsRound = "you lose, computer wins this round";
-let drawRound = "This is a draw";
-
-//function that defines the selection of computer symbol
-function computerPlay() {
-    let selection = Math.floor(Math.random() * 3);
-    if (selection === 2) {
-      return  selection = "rock";
-    } else if ( selection === 1) {
-       return  selection = "paper"
-     } else {
-       return selection = "scissors";
+function game(){
+    for (let i = 1; i <= 5; i++) {
+        playRound(i)
     }
+    logWins()
+} 
+
+function playRound(round) {
+const playerSelection = playerChoice()
+const computerSelection = computerChoice()
+const winner = checkWInner(playerSelection, computerSelection)
+winners.push(winner)
+logRound(playerSelection, computerSelection, winner, round)
 }
 
-// function that containins conditional on the outcome of each game round
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection){
-        return drawRound; 
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" ) {
-        return computerWinsRound
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        return computerWinsRound
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        return computerWinsRound
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        return humanWinsRound
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        return humanWinsRound
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        return humanWinsRound
-    } else{
-        return computerWinsRound
+//Make use of player input 
+function playerChoice() {
+let input = prompt("Make a choice between rock, paper, or scissors", "")
+while (input == null) {
+    input = prompt("Make a choice between rock, paper, or scissors", "")
+}
+input = input.toLowerCase()
+let check = validateInput(input)
+
+while (check == false){
+  input = prompt("Make sure you input rock, paper or scissors. spelling has to be exact but capitalization doesnt matter"
+  )
+
+while (input == null) {
+    input = prompt("Make a choice between rock, paper, or scissors", "")
+}
+input = input.toLowerCase()
+check = validateInput(input)
+}
+return input
+}
+
+//This function helps the computer to pick an item out of the choices array
+function computerChoice() {
+return choices[Math.floor(Math.random()*choices.length)]
+}
+
+//This function checks to see if user input rock, paper or scissors
+function validateInput(choice){
+    return choices.includes(choice)
     }
+
+    function checkWInner(choiceP, choiceC) {
+        if (choiceP == choiceC){
+            return "Tie"
+        } else if ( 
+        (choiceP == "rock" && choiceC == "scissors") || 
+        (choiceP == "paper" && choiceC == "rock") || 
+        (choiceP == "scissors" && choiceC == "paper")
+        ){
+            return "Player"
+        } else {
+        return "Computer"
+        }
+    }
+
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length
+    let computerWins = winners.filter((item) => item == "Computer").length
+    let ties = winners.filter((item) => item == "Tie").length
+    console.log("Results: ")
+    console.log("Player wins: ", playerWins)
+    console.log("Computer wins: ", computerWins)
+    console.log("Ties: ", ties)
 }
 
-const computerSelection = computerPlay();
-const playerSelection = "rock";
-
-function game() {
-    const playerSelection = prompt("Choose your preferred symbol. Rock, Paper or scissors?", "");
-    const computerSelection = computerPlay();
-     console.log(playRound(playerSelection, computerSelection));
+function logRound(playerChoice, computerChoice, winner, round) {
+    console.log("Round: ", round)
+    console.log("Player selected: ", playerChoice)
+    console.log("Computer selected: ", computerChoice)
+    if (winner == "Tie") {
+        console.log("Tie round")
+    } else {
+        console.log(winner, " won this round")
+    }
+    console.log("..................................")
 }
 
-let result = playRound(playerSelection, computerSelection)
-  
-// allows each round to be repeated five times
-for (let index = 0; index < 5; index++) {
-    game();
-  }
-  
-
-  function last(){
-     if (result = computerWinsRound){
-    return computerScore++
-  } else if (result = humanWinsRound){
-    return  humanScore++
-  } else if (result = drawRound) {
-    return draw++
-  } 
-}
-last();
-  
-
-function fat() {
-    if (computerScore++ > humanScore++) {
-        console.log("you lost, computer beat you")
-    } else if (humanScore++ > computerScore++){
-        console.log("champion, you beat computer")
-    }else if (computerScore++ == humanScore++) (
-        console.log("It ends in a tie")
-    )
-}
-fat();
+game()
